8cb5eb9e3661d2b0ea6ff2e7fe1f966c2d719480
Shon Pierce <Shon Pierce@10.0.0.43>
2021-09-05 23:25:26 -0400

    Adds document notes to Fibonacci.java class

--- Mod2_CEN4802C/src/Fibonacci.java
+++ Mod2_CEN4802C/src/Fibonacci.java
@@ -1,4 +1,43 @@
 
+/**
+ * Date: September 4, 2021
+ * @author Shon Pierce
+ * @version 1.0
+ * 
+ *<p>
+ *This method accepts n as a parameter. The recursive function takes in a number and 
+ *checks for 1 or 0. When input(n) is greater than 1(r) or 0(y) the function calls it self 
+ *recursively until the loops terminating condition is meet.       
+ * 
+ *@param fibonacciIteration  
+ *@returns an nth term of the fibonacci sequence 
+ *
+ *	static int fibonacciIteration(int n){
+		int r = 1;
+		int y =0;
+		
+		switch(n) {
+		case 0:
+			r = 0;
+			break;
+		
+		case 1:
+			r = 1;
+			break;
+		
+		default:
+			for(int i = 1; i < n; i++) {
+				r += y;
+				y = r - y;
+			}
+			break;
+		}
+		
+		return r;
+	}
+ *
+ */
+
 public class Fibonacci {
 	
 	public static void main(String [] args) {
@@ -36,7 +75,4 @@ public class Fibonacci {
 } // end class
 
 
-//naive recursive algorithm
-	// if n <= 2: f=1
-	//else f = fib(n-1) + fib(n-2)
-	// return return f
+
